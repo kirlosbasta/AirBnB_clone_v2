68e2aa5842304581fc1f8e55094ba392ab710c05
kirlos basta <kirlosbasta@gmail.com>
2024-03-20 00:20:06 +0200

    Add  test_console.py

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] == '{' and pline[-1] =='}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -124,18 +124,18 @@ class HBNBCommand(cmd.Cmd):
             return
         new_instance = HBNBCommand.classes[args[0]]()
         params = args[1:]
-        #seperate the parameters by =
+        # seperate the parameters by =
         params = [param.partition('=') for param in params]
         if params:
             for param in params:
                 try:
-                    # check for string 
+                    # check for string
                     if '\"' in param[2]:
                         val = param[2].replace('\"', '')
                         val = val.replace('_', ' ')
                         setattr(new_instance, param[0], val)
                     else:
-                        #set integer and floats
+                        # set integer and floats
                         setattr(new_instance, param[0], eval(param[2]))
                 except Exception as e:
                     pass
@@ -205,7 +205,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -337,6 +337,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
---
+++ tests/test_console.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+'''Test suite for AirBnB console'''
+import unittest
+from unittest.mock import patch
+from console import HBNBCommand
+from models import storage
+from io import StringIO
+import os
+
+
+class test_Console(unittest.TestCase):
+    '''Class for testing the console'''
+    def setUp(self) -> None:
+        ''' '''
+        pass
+    
+    def tearDown(self):
+        '''tear down'''
+        try:
+            os.remove('file.json')
+        except Exception:
+            pass
+
+    def test_create_no_args(self):
+        '''test for create without args'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create")
+            self.assertEqual(f.getvalue(), '** class name missing **\n')
+
+    def test_creat_cls(self):
+        '''test create with a class'''
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd('create User')
+            self.assertTrue(os.access('file.json', os.R_OK))
+
+    def test_create_param(self):
+        '''test with param'''
+        val = 'koko'
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd('create User name="koko"')
+        storage.reload()
+        user_key = storage.all()['User.' + f.getvalue().strip()]
+        self.assertEqual(user_key.__dict__['name'], val)
+
+    def test_create_string_space(self):
+        '''string with underscore'''
+        val = 'koko basta'
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd('create User name="koko_basta"')
+        storage.reload()
+        user_key = storage.all()['User.' + f.getvalue().strip()]
+        self.assertEqual(user_key.__dict__['name'], val)
+
+    def test_create_integer(self):
+        '''create integer'''
+        val = 23
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd('create User name=23')
+        storage.reload()
+        user_key = storage.all()['User.' + f.getvalue().strip()]
+        self.assertEqual(user_key.__dict__['name'], val)
+
+    def test_create_float(self):
+        '''create float'''
+        val = 23.8
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd('create User name=23.8')
+        storage.reload()
+        user_key = storage.all()['User.' + f.getvalue().strip()]
+        self.assertEqual(user_key.__dict__['name'], val)
