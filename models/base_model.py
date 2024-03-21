7117d210d7b4de296dab4ca675422fccf747ff01
kirlos basta <kirlosbasta@gmail.com>
2024-03-21 04:30:28 +0200

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -2,16 +2,6 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-<<<<<<< HEAD
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-import models
-
-
-Base = declarative_base()
-
-=======
->>>>>>> parent of 5567daf (Add all)
 
 
 class BaseModel:
