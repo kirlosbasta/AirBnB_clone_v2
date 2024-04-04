14de5d1d060bbaab77e852bb07ecbad4d4b9e7fa
kirlos basta <kirlosbasta@gmail.com>
2024-04-04 23:49:20 +0200

    Add 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,5 +10,5 @@ def do_pack():
     '''generates a .tgz archive from the contents of the web_static folder'''
     now = datetime.now().strftime('%Y%m%d%H%M%S')
     local('mkdir versions')
-    local(f'tar -cfavz ./versions/web_static_{now}.tgz web_static')
+    local(f'tar -cfvz versions/web_static_{now}.tgz web_static')
     return abspath('versions/web_static_{now}.tgz')
