277875ea5ccfc899aa3f95e6bedf4b6e37275427
kirlos basta <kirlosbasta@gmail.com>
2024-04-07 03:48:00 +0200

    Add 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -36,6 +36,8 @@ def do_deploy(archive_path):
         ))
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
             filename, filename_wex))
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(filename_wex, filename_wex))
         run('rm -rf /tmp/{}'.format(filename))
         run('rm -rf /data/web_static/current')
         run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
