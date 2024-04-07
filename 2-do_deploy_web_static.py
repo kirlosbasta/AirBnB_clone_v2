47b2fc2c84a16991194e18ed1109b27df74cfb7d
kirlos basta <kirlosbasta@gmail.com>
2024-04-07 04:24:55 +0200

    Add 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 '''Fabric script that distributes an archive to web servers,
 using the function do_deploy'''
+from datetime import datetime
 from fabric.api import *
 from os.path import isdir, exists
 
 
 env.hosts = ['100.25.19.62', '100.26.174.180']
+
+
+def do_pack():
+    """archive from the contents of the web_static folder of
+    AirBnB Clone repo"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
@@ -14,21 +29,26 @@ def do_deploy(archive_path):
         return False
     filename = archive_path.split('/')[-1]
     filename_wex = filename.partition('.')[0]
-    try:
-        put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}'.format(
-            filename_wex
-        ))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-            filename, filename_wex))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(filename_wex, filename_wex))
-        run('rm -rf /tmp/{}'.format(filename))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
-            filename_wex))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
-            format(filename_wex))
-        return True
-    except Exception:
+    path = '/data/web_static/releases/'
+    if put(archive_path, '/tmp/').failed is True:
         return False
+    if run('mkdir -p {}{}'.format(
+            path, filename_wex)).failed is True:
+        return False
+    if run('tar -xzf /tmp/{} -C {}{}/'.format(
+            filename, path, filename_wex)).failed is True:
+        return False
+    if run('mv {0}{1}/web_static/* \
+            {0}{1}/'.format(path, filename_wex)).failed is True:
+        return False
+    if run('rm -rf /tmp/{}'.format(filename)).failed is True:
+        return False
+    if run('rm -rf {}{}/web_static'.format(
+            path, filename_wex)).failed is True:
+        return False
+    if run('rm -rf /data/web_static/current').failed is True:
+        return False
+    if run('ln -s {}{}/ /data/web_static/current'.
+            format(path, filename_wex)).failed is True:
+        return False
+    return True
