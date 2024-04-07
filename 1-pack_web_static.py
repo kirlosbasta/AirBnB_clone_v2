30b025402119eebeb28f847504f761bb0c8cb59e
kirlos basta <kirlosbasta@gmail.com>
2024-04-07 03:30:19 +0200

    Add 2-do_deploy_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,7 +7,8 @@ from os.path import isdir
 
 
 def do_pack():
-    """generates a tgz archive"""
+    """archive from the contents of the web_static folder of
+    AirBnB Clone repo"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''Fabric script that distributes an archive to web servers,
+using the function do_deploy'''
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir, exists
+
+env.hosts = ['100.25.19.62', '100.26.174.180']
+
+def do_pack():
+    """archive from the contents of the web_static folder of
+    AirBnB Clone repo"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
+    
+def do_deploy(archive_path):
+    '''deploy an archive to web server'''
+    if exists(archive_path) is False:
+        return False
+    filename = archive_path.split('/')[-1]
+    filename_wex = filename.partition('.')[0]
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/releases/{}'.format(
+            filename_wex
+        ))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, filename_wex))
+        run('rm -rf /tmp/{}'.format(filename))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(filename_wex))
+    except:
+        return False
