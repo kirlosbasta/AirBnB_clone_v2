a5471dffaa169a47fa9146e70479826321e1e4be
kirlos basta <kirlosbasta@gmail.com>
2024-04-07 16:27:13 +0200

    Add 3-deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,31 +24,21 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    '''deploy an archive to web server'''
+    """distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
-    filename = archive_path.split('/')[-1]
-    filename_wex = filename.split('.')[0]
-    path = '/data/web_static/releases/'
-    if put(archive_path, '/tmp/').failed is True:
+    try:
+        file = archive_path.split("/")[-1]
+        file_noEx = file.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, file_noEx))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file, path, file_noEx))
+        run('rm /tmp/{}'.format(file))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_noEx))
+        run('rm -rf {}{}/web_static'.format(path, file_noEx))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, file_noEx))
+        return True
+    except:
         return False
-    if run('mkdir -p {}{}'.format(
-            path, filename_wex)).failed is True:
-        return False
-    if run('tar -xzf /tmp/{} -C {}{}/'.format(
-            filename, path, filename_wex)).failed is True:
-        return False
-    if run('mv {0}{1}/web_static/* {0}{1}/'.format(
-                        path, filename_wex)).failed is True:
-        return False
-    if run('rm -rf /tmp/{}'.format(filename)).failed is True:
-        return False
-    if run('rm -rf {}{}/web_static'.format(
-            path, filename_wex)).failed is True:
-        return False
-    if run('rm -rf /data/web_static/current').failed is True:
-        return False
-    if run('ln -s {}{}/ /data/web_static/current'.
-            format(path, filename_wex)).failed is True:
-        return False
-    return True
