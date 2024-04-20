7a05f2a99fed10b53fd76349344047aa6f63ff16
kirlos basta <kirlosbasta@gmail.com>
2024-04-21 01:40:02 +0200

    Add 5-number_template.py

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+'''script that starts a Flask web application'''
+from flask import Flask, render_template
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''return Hello HBNB! for root URL'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''return HBNB for /hbnb URL'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    '''return C followed by text for /c/<text> URL'''
+    return f"C {escape(text).replace('_', ' ')}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_route(text='is cool'):
+    '''return Python followed by text for /python/<text> URL'''
+    return f"Python {escape(text).replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    '''return n is a number for /number/<n> URL'''
+    return f"{escape(n)} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    '''return html with Number is followed by n /number_template/<n> URL'''
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
