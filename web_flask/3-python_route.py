572628ec9d50dd8d1a87d3df08c7c6bb4c3d7201
kirlos basta <kirlosbasta@gmail.com>
2024-04-21 01:12:51 +0200

    Add 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''script that starts a Flask web application'''
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''return Hello HBNB! for root URL'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''return HBNB for /hbnb URL'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    '''return C followed by text for /c/<text> URL'''
+    return f"C {escape(text).replace('_', ' ')}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_route(text='is cool'):
+    '''return Python followed by text for /python/<text> URL'''
+    return f"Python {escape(text).replace('_', ' ')}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
